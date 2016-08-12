57c9936fb4cb76148709e8085b6f5553b03439ba
Matheus Gadelha <matheusabrantesgadelha@gmail.com>
2016-08-12 18:04:02 -0400

    Added checkpoint saving.

--- RenderNet.py
+++ RenderNet.py
@@ -1,13 +1,13 @@
 import numpy as np
 import tensorflow as tf
-import matplotlib.pyplot as plt
 import ops
 import glob
+import os
 
 
 class RenderNet:
 
-    def __init__(self, image_size=(64, 64), input_size=23, n_iterations=500, batch_size=64, lrate=0.001):
+    def __init__(self, image_size=(64, 64), input_size=23, n_iterations=50, batch_size=64, lrate=0.001):
         self.image_size = image_size
         self.input_size = input_size
         self.n_pixels = self.image_size[0] * self.image_size[1] * 3  # number of channels
@@ -26,6 +26,8 @@ class RenderNet:
 
         self.optimizer = tf.train.AdamOptimizer(self.lrate).minimize(self.loss)
 
+        self.saver = tf.train.Saver()
+
     def train(self):
         dataset_files = np.array(glob.glob("data/*.png"))
         dataset_params = np.load("data_params.npy")
@@ -33,24 +35,48 @@ class RenderNet:
         n_files = dataset_params.shape[0]
 
         testset_idxs = np.random.choice(range(n_files), self.batch_size)
+        test_imgs = ops.load_flatten_imgbatch(dataset_files[testset_idxs])
+        training_step = 0
 
         with tf.Session() as sess:
             sess.run(tf.initialize_all_variables())
-            for epoch in range(self.n_iterations):
+            for epoch in xrange(self.n_iterations):
 
                 rand_idxs = np.random.permutation(range(n_files))
                 n_batches = n_files // self.batch_size
 
-                for batch_i in range(n_batches):
+                for batch_i in xrange(n_batches):
                     idxs_i = rand_idxs[batch_i * self.batch_size: (batch_i + 1) * self.batch_size]
                     imgs_batch = ops.load_flatten_imgbatch(dataset_files[idxs_i])
                     sess.run(self.optimizer, feed_dict={self.img_params: dataset_params[idxs_i, :],
                                                         self.final_image: imgs_batch})
+                    training_step += 1
 
-                test_imgs = ops.load_flatten_imgbatch(dataset_files[testset_idxs])
-                current_loss = sess.run(self.loss, feed_dict={self.img_params: dataset_params[testset_idxs, :],
-                                                        self.final_image: test_imgs})
-                print "EPOCH ", epoch, ", Loss: ", current_loss
+                    current_loss = sess.run(self.loss, feed_dict={self.img_params: dataset_params[testset_idxs, :],
+                                                                  self.final_image: test_imgs})
+
+                    print "Epoch {}/{}, Batch {}/{}, Loss {}".format(epoch + 1, self.n_iterations,
+                                                                     batch_i + 1, n_batches, current_loss)
+
+                    ops.show_graph_operations()
+
+                # Save checkpoint
+                if not os.path.exists("checkpoint"):
+                    print "Checkpoint folder not found. Creating one..."
+                    os.makedirs("checkpoint")
+                    print "Done."
+
+                self.saver.save(sess, 'checkpoint/model.ckpt', global_step=training_step)
+
+    def forward(self, render_params):
+        ckpt = tf.train.get_checkpoint_state("checkpoint")
+        if ckpt and ckpt.model_checkpoint_path:
+            self.saver.restore(sess, ckpt.model_checkpoint_path)
+        else:
+            print "No saved model found. Train the network first."
+
+        img = np.array(self.prediction.eval(feed_dict={self.img_params: render_params}))
+        return img.reshape((self.img.size[0], self.img.size[1], 3))
 
 
     def architecture_check(self):
