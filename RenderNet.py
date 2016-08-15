651bfda2fc0e09bee4f217b7a6d561d5b768c4ae
Matheus Gadelha <matheusabrantesgadelha@gmail.com>
2016-08-15 17:34:47 -0400

    Changed architecture to a convolutional one. Fixed bug with training images by sorting file names.

--- RenderNet.py
+++ RenderNet.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 import ops
 import glob
 import os
@@ -7,77 +8,87 @@ import os
 
 class RenderNet:
 
-    def __init__(self, image_size=(64, 64), input_size=23, n_iterations=50, batch_size=64, lrate=0.001):
+    def __init__(self, sess=tf.Session(), image_size=(64, 64), input_size=23, n_iterations=50, batch_size=64, lrate=0.001):
         self.image_size = image_size
         self.input_size = input_size
         self.n_pixels = self.image_size[0] * self.image_size[1] * 3  # number of channels
         self.n_iterations = n_iterations
         self.batch_size = batch_size
         self.lrate = lrate
+        self.session = sess
+        self.base_dim = 1024
 
         # Network architecture
-        self.img_params = tf.placeholder(tf.float32, shape=[None, self.input_size], name='input')
-        self.final_image = tf.placeholder(tf.float32, shape=[None, self.n_pixels], name='final_image')
+        with tf.variable_scope("rendernet"):
+            self.img_params = tf.placeholder(tf.float32, shape=[batch_size, self.input_size], name='input')
+            self.final_image = tf.placeholder(tf.float32, shape=[batch_size, image_size[0], image_size[1], 3], name='final_image')
 
-        self.h1 = ops.linear(self.img_params, self.input_size, self.n_pixels, activation=tf.nn.tanh, scope='h1')
-        self.prediction = ops.linear(self.h1, self.n_pixels, self.n_pixels, scope='prediction')
+            self.h1 = ops.linear(self.img_params, self.input_size, 1024, activation=tf.nn.relu, scope='h1')
+            self.h2 = tf.reshape(ops.linear(self.h1, 1024, self.base_dim * 4 * 4, activation=tf.nn.relu, scope='h2'),
+                                 [-1, 4, 4, self.base_dim])
+            self.h3 = tf.nn.relu(ops.deconv2d(self.h2, [self.batch_size, 8, 8, self.base_dim/2], name='h3'))
+            self.h4 = tf.nn.relu(ops.deconv2d(self.h3, [self.batch_size, 16, 16, self.base_dim/4], name='h4'))
+            self.h5 = tf.nn.relu(ops.deconv2d(self.h4, [self.batch_size, 32, 32, self.base_dim/8], name='h5'))
+            self.prediction = tf.nn.tanh(ops.deconv2d(self.h5, [self.batch_size, 64, 64, 3], name='prediction'))
 
-        self.loss = tf.reduce_mean(ops.l2norm(self.prediction, self.final_image))
+            self.loss = tf.reduce_mean(ops.l2norm_sqrd(self.prediction, self.final_image))
 
-        self.optimizer = tf.train.AdamOptimizer(self.lrate).minimize(self.loss)
+            self.optimizer = tf.train.AdamOptimizer(self.lrate).minimize(self.loss)
 
-        self.saver = tf.train.Saver()
+            self.saver = tf.train.Saver()
 
     def train(self):
-        dataset_files = np.array(glob.glob("data/*.png"))
+        dataset_files = glob.glob("data/*.png")
+        dataset_files.sort(key=ops.alphanum_key)
+        dataset_files = np.array(dataset_files)
         dataset_params = np.load("data_params.npy")
 
         n_files = dataset_params.shape[0]
 
         testset_idxs = np.random.choice(range(n_files), self.batch_size)
-        test_imgs = ops.load_flatten_imgbatch(dataset_files[testset_idxs])
+        test_imgs = ops.load_imgbatch(dataset_files[testset_idxs])
         training_step = 0
 
-        with tf.Session() as sess:
-            sess.run(tf.initialize_all_variables())
-            for epoch in xrange(self.n_iterations):
+        self.session.run(tf.initialize_all_variables())
+        for epoch in xrange(self.n_iterations):
 
-                rand_idxs = np.random.permutation(range(n_files))
-                n_batches = n_files // self.batch_size
+            rand_idxs = np.random.permutation(range(n_files))
+            n_batches = n_files // self.batch_size
 
-                for batch_i in xrange(n_batches):
-                    idxs_i = rand_idxs[batch_i * self.batch_size: (batch_i + 1) * self.batch_size]
-                    imgs_batch = ops.load_flatten_imgbatch(dataset_files[idxs_i])
-                    sess.run(self.optimizer, feed_dict={self.img_params: dataset_params[idxs_i, :],
-                                                        self.final_image: imgs_batch})
-                    training_step += 1
+            for batch_i in xrange(n_batches):
+                idxs_i = rand_idxs[batch_i * self.batch_size: (batch_i + 1) * self.batch_size]
+                imgs_batch = ops.load_imgbatch(dataset_files[idxs_i])
+                self.session.run(self.optimizer, feed_dict={self.img_params: dataset_params[idxs_i, :],
+                                                    self.final_image: imgs_batch})
+                training_step += 1
 
-                    current_loss = sess.run(self.loss, feed_dict={self.img_params: dataset_params[testset_idxs, :],
-                                                                  self.final_image: test_imgs})
+                current_loss = self.session.run(self.loss, feed_dict={self.img_params: dataset_params[testset_idxs, :],
+                                                              self.final_image: test_imgs})
 
-                    print "Epoch {}/{}, Batch {}/{}, Loss {}".format(epoch + 1, self.n_iterations,
-                                                                     batch_i + 1, n_batches, current_loss)
+                print "Epoch {}/{}, Batch {}/{}, Loss {}".format(epoch + 1, self.n_iterations,
+                                                                 batch_i + 1, n_batches, current_loss)
 
-                    ops.show_graph_operations()
-
+                # ops.show_graph_operations()
                 # Save checkpoint
-                if not os.path.exists("checkpoint"):
-                    print "Checkpoint folder not found. Creating one..."
-                    os.makedirs("checkpoint")
-                    print "Done."
-
-                self.saver.save(sess, 'checkpoint/model.ckpt', global_step=training_step)
+                if training_step % 100 == 0:
+                    if not os.path.exists("checkpoint"):
+                        print "Checkpoint folder not found. Creating one..."
+                        os.makedirs("checkpoint")
+                        print "Done."
+                    self.saver.save(self.session, 'checkpoint/model.ckpt', global_step=training_step)
 
     def forward(self, render_params):
         ckpt = tf.train.get_checkpoint_state("checkpoint")
         if ckpt and ckpt.model_checkpoint_path:
-            self.saver.restore(sess, ckpt.model_checkpoint_path)
+            self.saver.restore(self.session, ckpt.model_checkpoint_path)
         else:
             print "No saved model found. Train the network first."
 
-        img = np.array(self.prediction.eval(feed_dict={self.img_params: render_params}))
-        return img.reshape((self.img.size[0], self.img.size[1], 3))
-
+        img = np.array(self.prediction.eval(session=self.session, feed_dict={self.img_params: render_params}))
+        print img.shape
+        plt.imshow(img[63,:,:,:])
+        plt.show()
+        return img
 
     def architecture_check(self):
         with tf.Session() as sess:
@@ -87,7 +98,9 @@ class RenderNet:
 
 if __name__ == '__main__':
     rnet = RenderNet()
-    # rnet.architecture_check()
+    #rnet.architecture_check()
+    #data = np.load("data_params.npy")[10000:10064, :]
     rnet.train()
+    #rnet.forward(data)
 
 
--- ops.py
+++ ops.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import matplotlib.image as mpimg
 import numpy as np
+import re
 
 
 def linear(x, n_input, n_output, activation=None, scope=None):
@@ -19,7 +20,34 @@ def linear(x, n_input, n_output, activation=None, scope=None):
         return h
 
 
-def l2norm(a, b): return tf.sqrt(tf.reduce_sum(tf.pow(a-b, 2), 1))
+# From DCGAN tensrorflow implementation
+def deconv2d(input_, output_shape,
+             k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
+             name="deconv2d", with_w=False):
+    with tf.variable_scope(name):
+        # filter : [height, width, output_channels, in_channels]
+        w = tf.get_variable('w', [k_h, k_h, output_shape[-1], input_.get_shape()[-1]],
+                            initializer=tf.random_normal_initializer(stddev=stddev))
+
+        try:
+            deconv = tf.nn.conv2d_transpose(input_, w, output_shape=output_shape,
+                                            strides=[1, d_h, d_w, 1])
+
+        # Support for verisons of TensorFlow before 0.7.0
+        except AttributeError:
+            deconv = tf.nn.deconv2d(input_, w, output_shape=output_shape,
+                                    strides=[1, d_h, d_w, 1])
+
+        biases = tf.get_variable('biases', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
+        deconv = tf.reshape(tf.nn.bias_add(deconv, biases), deconv.get_shape())
+
+        if with_w:
+            return deconv, w, biases
+        else:
+            return deconv
+
+
+def l2norm_sqrd(a, b): return tf.reduce_sum(tf.pow(a-b, 2), 1)
 
 
 def show_graph_operations():
@@ -33,3 +61,23 @@ def load_flatten_imgbatch(img_paths):
         images.append(mpimg.imread(path).flatten())
     return np.array(images)
 
+
+def load_imgbatch(img_paths):
+    images = []
+    for path in img_paths:
+        images.append(mpimg.imread(path))
+    return np.array(images)
+
+
+def tryint(s):
+    try:
+        return int(s)
+    except:
+        return s
+
+
+def alphanum_key(s):
+    """ Turn a string into a list of string and number chunks.
+        "z23a" -> ["z", 23, "a"]
+    """
+    return [tryint(c) for c in re.split('([0-9]+)', s)]
