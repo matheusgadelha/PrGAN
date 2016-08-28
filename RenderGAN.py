7c4831cbf76b7b2a8bb8deadf59f59315e114acc
Matheus Gadelha <matheusabrantesgadelha@gmail.com>
2016-08-28 02:17:08 -0400

    Changed optimizer and parameters.

--- RenderGAN.py
+++ RenderGAN.py
@@ -9,7 +9,7 @@ from RenderNet import RenderNet
 class RenderGAN:
 
     def __init__(self, sess=tf.Session(), image_size=(64, 64), z_size=10,
-                 n_iterations=50, batch_size=64, lrate=0.001, d_size=64):
+                 n_iterations=50, batch_size=64, lrate=0.005, d_size=64):
 
         self.image_size = image_size
         self.n_pixels = self.image_size[0] * self.image_size[1] * 3  # number of channels
@@ -23,6 +23,10 @@ class RenderGAN:
 
         self.rendernet = RenderNet(sess=self.session, image_size=self.image_size,
                                    batch_size=self.batch_size)
+
+        self.g_bn0 = ops.batch_norm(name='g_bn0')
+        self.g_bn1 = ops.batch_norm(name='g_bn1')
+        self.g_bn2 = ops.batch_norm(name='g_bn2')
 
         with tf.variable_scope('gan'):
             self.images = tf.placeholder(tf.float32, shape=[batch_size, image_size[0], image_size[1], 3],
@@ -47,8 +51,8 @@ class RenderGAN:
             self.D_vars = [v for v in allvars if 'd_' in v.name]
             self.G_vars = [v for v in allvars if 'g_' in v.name]
 
-            self.D_optim = tf.train.AdamOptimizer(self.lrate).minimize(self.D_loss, var_list=self.D_vars)
-            self.G_optim = tf.train.AdamOptimizer(self.lrate).minimize(self.G_loss, var_list=self.G_vars)
+            self.D_optim = tf.train.GradientDescentOptimizer(self.lrate).minimize(self.D_loss, var_list=self.D_vars)
+            self.G_optim = tf.train.GradientDescentOptimizer(self.lrate).minimize(self.G_loss, var_list=self.G_vars)
 
             self.saver = tf.train.Saver()
 
@@ -60,6 +64,7 @@ class RenderGAN:
         dataset_files = glob.glob("data/gan/*.png")
         dataset_files = np.array(dataset_files)
         n_files = dataset_files.shape[0]
+        sample_z = np.random.uniform(-1, 1, [self.batch_size, self.z_size])
 
         self.session.run(tf.initialize_all_variables())
         self.rendernet.load('checkpoint')
@@ -71,18 +76,31 @@ class RenderGAN:
             for batch_i in xrange(n_batches):
                 idxs_i = rand_idxs[batch_i * self.batch_size: (batch_i + 1) * self.batch_size]
                 imgs_batch = ops.load_imgbatch(dataset_files[idxs_i])
-                batch_z = np.random.uniform(-1, 1, [self.batch_size, self.z_size]) \
-                            .astype(np.float32)
-
-                # Update discriminator
-                self.session.run(self.D_optim, feed_dict={self.images: imgs_batch, self.z: batch_z})
-                # Update generator
-                for i in xrange(5):
-                    self.session.run(self.G_optim, feed_dict={self.z: batch_z})
+                batch_z = np.random.uniform(-1, 1, [self.batch_size, self.z_size])
 
                 dloss_fake = self.D_loss_fake.eval(session=self.session, feed_dict={self.z: batch_z})
                 dloss_real = self.D_loss_real.eval(session=self.session, feed_dict={self.images: imgs_batch})
                 gloss = self.G_loss.eval(session=self.session, feed_dict={self.z: batch_z})
+
+                train_discriminator = True
+                train_generator = True
+
+                margin = 0.3
+                if dloss_fake < margin or dloss_real < margin:
+                    train_discriminator = False
+                if dloss_fake > 1.0-margin or dloss_real > 1.0-margin:
+                    train_generator = False
+                if train_discriminator is False and train_generator is False:
+                    train_generator = train_discriminator = True
+
+                # Update discriminator
+                if train_discriminator:
+                    self.session.run(self.D_optim, feed_dict={self.images: imgs_batch, self.z: batch_z})
+                # Update generator
+                if train_generator:
+                    for i in xrange(5):
+                        self.session.run(self.G_optim, feed_dict={self.z: batch_z})
+
 
 #                test_params = np.zeros((64,23))
 #                test_params[0, :] = np.array([5, 5, 5,
@@ -93,10 +111,11 @@ class RenderGAN:
 #                                        0, 1, 0,
 #                                        0, 1, 1,
 #                                        1, 1, 1])
-#
-                rendered_images = self.G.eval(session=self.session, feed_dict={self.z: batch_z})
-                rendered_images = np.array(rendered_images)
-                ops.save_images(rendered_images, [8, 8], "gan_results.png")
+
+                if batch_i % 10 == 0:
+                    rendered_images = self.G.eval(session=self.session, feed_dict={self.z: sample_z})
+                    rendered_images = np.array(rendered_images)
+                    ops.save_images(rendered_images, [8, 8], "results/gancubes{}.png".format(epoch*n_batches+batch_i))
 
                 print "EPOCH[{}], BATCH[{}/{}]".format(epoch, batch_i, n_batches)
                 print "Discriminator Loss - Real:{} / Fake:{} - Total:{}".format(dloss_real, dloss_fake,
@@ -119,7 +138,10 @@ class RenderGAN:
         with tf.variable_scope('gan'):
             h0 = ops.linear(z_enc, self.z_size, 256, activation=ops.lrelu, scope='g_h0')
             h1 = ops.linear(h0, 256, 256, activation=ops.lrelu, scope='g_h1')
-            self.img_params = ops.linear(h1, 256, self.rendernet.input_size, scope='g_img_params')
+            h2 = ops.linear(h1, 256, 256, activation=ops.lrelu, scope='g_h2')
+            h3 = ops.linear(h2, 256, 256, activation=ops.lrelu, scope='g_h3')
+            h4 = ops.linear(h3, 256, 256, activation=ops.lrelu, scope='g_h4')
+            self.img_params = ops.linear(h4, 256, self.rendernet.input_size, scope='g_img_params')
 
         with tf.variable_scope('rendernet'):
             rendered_img = self.rendernet.render(self.img_params, reuse=True)
