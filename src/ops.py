0a92e2e58e2fa1fd8eb241aa05fcee50a1843c05
Matheus Gadelha <matheusabrantesgadelha@gmail.com>
2016-09-16 19:12:50 -0400

    Added minibatch discrimination to ops.

--- src/ops.py
+++ src/ops.py
@@ -43,6 +43,53 @@ class batch_norm(object):
         return normed
 
 
+class batch_norm_linear(object):
+    """Code modification of http://stackoverflow.com/a/33950177"""
+    def __init__(self, epsilon=1e-5, momentum=0.9, name="batch_norm"):
+        with tf.variable_scope(name):
+            self.epsilon = epsilon
+            self.momentum = momentum
+
+            self.ema = tf.train.ExponentialMovingAverage(decay=self.momentum)
+            self.name = name
+
+    def __call__(self, x, train=True):
+        shape = x.get_shape().as_list()
+
+        if train:
+            with tf.variable_scope(self.name):
+                self.beta = tf.get_variable("beta", [shape[-1]],
+                                            initializer=tf.constant_initializer(0.))
+                self.gamma = tf.get_variable("gamma", [shape[-1]],
+                                             initializer=tf.random_normal_initializer(1., 0.02))
+
+                # Remove 2 from list to work with fully connected
+                # TODO: fix this issue
+                batch_mean, batch_var = tf.nn.moments(x, [0, 1], name='moments')
+                ema_apply_op = self.ema.apply([batch_mean, batch_var])
+                self.ema_mean, self.ema_var = self.ema.average(batch_mean), self.ema.average(batch_var)
+
+                with tf.control_dependencies([ema_apply_op]):
+                    mean, var = tf.identity(batch_mean), tf.identity(batch_var)
+        else:
+            mean, var = self.ema_mean, self.ema_var
+
+        normed = tf.nn.batch_norm_with_global_normalization(
+                x, mean, var, self.beta, self.gamma, self.epsilon, scale_after_normalization=True)
+
+        return normed
+
+
+def minibatch(input, num_kernels=5, kernel_dim=3):
+    x = linear(input, num_kernels * kernel_dim)
+    activation = tf.reshape(x, (-1, num_kernels, kernel_dim))
+    diffs = tf.expand_dims(activation, 3) - \
+        tf.expand_dims(tf.transpose(activation, [1, 2, 0]), 0)
+    abs_diffs = tf.reduce_sum(tf.abs(diffs), 2)
+    minibatch_features = tf.reduce_sum(tf.exp(-abs_diffs), 2)
+    return tf.concat(1, [input, minibatch_features])
+
+
 def lrelu(x, leak=0.2, name="lrelu"):
     with tf.variable_scope(name):
         f1 = 0.5 * (1 + leak)
